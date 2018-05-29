36d78cb44a46bb5a75b8571990d291cc5f83e9c6
kumbhargv08 <kumbhargv08@gmail.com>
2018-05-29 22:52:54 +0530

    slack integration with ui

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from slackclient import SlackClient
+import time
 
 bot = ChatBot('Bot',read_only=True,
               logic_adapters=[
@@ -14,6 +16,8 @@ bot = ChatBot('Bot',read_only=True,
         }
     ],
     trainer='chatterbot.trainers.ListTrainer')
+slack_token = 'xoxp-13390904948-187170561730-371311368160-d98a24fe580696e394d7d392b310e406'
+sc = SlackClient(slack_token)
 
 app = Flask(__name__)
 
@@ -21,6 +25,32 @@ app = Flask(__name__)
 def get_answer( question ):
     print( question )
     reply = bot.get_response(question)
+    if question == reply or reply=='I am sorry, but I do not understand.':
+        sc.api_call(
+            "chat.postMessage",
+            channel="#pp",
+            text="Hi Vidya/Monali!!! Please answer following question"
+            )
+        if sc.rtm_connect():
+            while True:
+                result = sc.rtm_read()
+                if(len(result)):
+                    set1 = result[0]
+                    print(set1)
+                    if(len(set1) and set1["type"]=="message"):
+                        print("type is : " , type(set1))
+                        response = set1["text"]         #response=<bytecode> message
+                        response = response.split("> ")[1] if response.__contains__('>') else response
+                        print("response is:",response)
+                        bot.train([question,response,])
+                        reply = response
+                        break
+                #print("result is: " , result)
+                time.sleep(1)
+        else:
+            print("unable to connect to slack chatbot")             
+    else:
+        print('Chatbot:',reply)
     return jsonify({'answer': str( reply )}), 201
 
 
